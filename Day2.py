cb80e017441dbbed0d74156c5b891f7036c968f7
Raymond Zeng <shilei.zeng98@outlook.com>
2024-12-02 00:24:04 -0500

    Day2

---
+++ Day2.py
@@ -0,0 +1,27 @@
+file = open("Day2.txt", "r")
+
+inputs = []
+
+for line in file:
+    array = list(map(int, line.split()))
+    inputs.append(array)
+
+total_safe = 0
+
+def rules(levels):
+    increasing = None
+    for i in range(len(levels) - 1):
+        diff = levels[i + 1] - levels[i]
+        if abs(diff) < 1 or abs(diff) > 3:
+            return False
+        if increasing is None:
+            increasing = diff > 0
+        elif (diff > 0) != increasing:
+            return False
+    return True
+
+for levels in inputs:
+    if rules(levels):
+        total_safe += 1
+
+print(total_safe)
