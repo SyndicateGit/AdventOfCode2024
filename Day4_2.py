7afef00fb3b60ca30450555700b07faf4cfb5003
Raymond Zeng <shilei.zeng98@outlook.com>
2024-12-04 00:43:16 -0500

    Day4 part 2

---
+++ Day4_2.py
@@ -0,0 +1,20 @@
+file = open("Day4.txt", "r")
+
+grid = []
+
+for line in file:
+    row = line.strip()
+    grid.append(row)
+
+XMAS = "MAS"
+
+ans = 0
+for row in range(len(grid) - len(XMAS) + 1):
+    for col in range(len(grid[row]) - len(XMAS) + 1):
+        diag_str = ''.join([grid[row + i][col + i] for i in range(len(XMAS))])
+        diag_str2 = ''.join([grid[row + i][col + len(XMAS) - 1 - i] for i in range(len(XMAS))])
+        if diag_str2 == diag_str or diag_str2 == diag_str[::-1]:
+            ans += diag_str.count(XMAS)
+            ans += diag_str.count(XMAS[::-1])
+
+print (ans)
