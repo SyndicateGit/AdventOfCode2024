7d6d80b70a88bc12da25ee07ffedbcdcf4562e56
Raymond Zeng <shilei.zeng98@outlook.com>
2024-12-07 00:27:24 -0500

    Day7

---
+++ Day7.py
@@ -0,0 +1,30 @@
+file = open("Day7.txt", "r")
+
+inputs = []
+
+for line in file:
+    test = line.strip().split(" ")
+    inputs.append([int(test[0][:-1])])
+    inputs[-1].extend([int(x) for x in test[1:]])
+
+print(inputs)
+
+
+
+
+def check_test(numbers, total, index, curr):
+    if index == len(numbers):
+        if curr == total:
+            return True
+        return False
+    if check_test(numbers, total, index + 1, curr + numbers[index]) or check_test(numbers, total, index + 1, curr * numbers[index]):
+        return True
+
+sum = 0
+for test in inputs:
+    total = test[0]
+    numbers = test[1:]
+    if check_test(numbers, total, 1, numbers[0]):
+        sum += total
+
+print(sum)
